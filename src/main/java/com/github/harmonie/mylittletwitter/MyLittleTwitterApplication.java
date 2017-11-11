b63f1be253bfaa29f1daffd6db3a8f0466bb4214
Jean Champémont <git@jeanchampemont.com>
2017-11-11 20:46:21 +0100

    first commit

---
+++ src/main/java/com/github/harmonie/mylittletwitter/MyLittleTwitterApplication.java
@@ -0,0 +1,12 @@
+package com.github.harmonie.mylittletwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLittleTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLittleTwitterApplication.class, args);
+	}
+}
